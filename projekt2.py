caace390abbb5370e1a0c0601b224c4ce70287ca
jp <prudik.j@seznam.cz>
2022-05-15 08:39:53 +0200

    zacatek uvod

--- projekt2.py
+++ projekt2.py
@@ -5,3 +5,11 @@ author: Jaroslav Prudík
 email: prudik.j@seznam.cz
 discord: Jarek#3498
 """
+oddelovac = "-" * 48
+
+print("Hi there!")
+print(oddelovac)
+print("I've generated a random 4 digit number for you.\nLet's play a bulls and cows game")
+print(oddelovac)
+input("Enter a number:")
+print(oddelovac)
