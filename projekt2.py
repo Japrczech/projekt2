2b65fb14ce804cd0352b12e196f077ec7b9a1094
jp <prudik.j@seznam.cz>
2022-06-05 06:40:13 +0200

    zacatek uvod pokracovani další

--- projekt2.py
+++ projekt2.py
@@ -5,60 +5,110 @@ author: Jaroslav Prudík
 email: prudik.j@seznam.cz
 discord: Jarek#3498
 """
-#import balíčků (random = náhodný výběr)
+# import balíčků (random = náhodný výběr)
 import random
-#seznam pro výběr jednotlivých číslic. cislo1 pro první číslici bez 0
-cisla = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-cislo1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(random.choice(cisla))
-
-#základní definice a texty
+cow = 0
+bull = 0
+# základní definice a texty
 
 oddelovac = "-" * 48
-#jednotlivé číslice čtyřmístného náhodného čísla
-prvni_cislice = random.choice(cislo1)
-druha_cislice = random.choice(cisla)
-treti_cislice = random.choice(cisla)
-ctvrta_cislice = random.choice(cisla)
+#cow = 0
+#bull = 0
+# vygenerované náhodné číslo jako seznam jednotlivých číslic
+#nahodne_cislo = list(f"{int(prvni_cislice)}{int(druha_cislice)}{int(treti_cislice)}{int(ctvrta_cislice)}")
 
-#vygenerované náhodné číslo jako seznam jednotlivých číslic
-nahodne_cislo = list(f"{int(prvni_cislice)}{int(druha_cislice)}{int(treti_cislice)}{int(ctvrta_cislice)}")
+nahodne_cislo = list()
+
+while len(nahodne_cislo) < 4:
+    cislo = random.randint(1,9)
+    if cislo not in nahodne_cislo:
+        nahodne_cislo.append(cislo)
+
+prvni_cislice = nahodne_cislo[0]
+druha_cislice = nahodne_cislo[1]
+treti_cislice = nahodne_cislo[2]
+ctvrta_cislice = nahodne_cislo[3]
+nahodne_cislo = list(f"{prvni_cislice}{druha_cislice}{treti_cislice}{ctvrta_cislice}")
+
 print(nahodne_cislo)
 
-#úvodní text
+# úvodní text
 print("Hi there!")
 print(oddelovac)
 print("I've generated a random 4 digit number for you.\nLet's play a bulls and cows game")
 print(oddelovac)
 
-#začátek hry - zadání čísla, rozdělení na jednotlivé číslice do seznamu
-zadane_cislo1 = input("Enter a number:")
-#zajištění čtyřmístného čísla
-if len(zadane_cislo1) == 4:
-    print(oddelovac)
-else:
-    print("the number doesnt have four numbers")
-    quit()
-cislo1 = int(zadane_cislo1[0])
-cislo2 = int(zadane_cislo1[1])
-cislo3 = int(zadane_cislo1[2])
-cislo4 = int(zadane_cislo1[3])
-zadane_cislo1_seznam = list(f"{cislo1}{cislo2}{cislo3}{cislo4}")
-print(zadane_cislo1_seznam)
-cows_bulls = {"cows": 0, "buls": 0}
-def kontrola_cisel(zadane_cislo1):
-    for cislo in nahodne_cislo:
-        if cislo in zadane_cislo1_seznam:
-            cows_bulls["cows"] = +1
-kontrola_cisel(zadane_cislo1)
-print(cows_bulls)
-
-#if zadane_cislo1 not in nahodne_cislo:
-#   print("0 bulls, 0 cows")
-#elif zadane_cislo1 in nahodne_cislo:
-#   if zadane_cislo1[0] = nahodne_cislo[0]:
-#    bulls
-#for cislo in nahodne_cislo and zadane_cislo1_seznam
+# začátek hry - zadání čísla, rozdělení na jednotlivé číslice do seznamu
 
 
-#každou číslici kontrolovat zvlášť tj. shodnost a pozici
+#cow = 0
+#bull = 0
+
+#def zadani_cisla(zadane_cislo1_seznam):
+#zadane_cislo1_seznam = list(f"{zadane_cislo1[0]}{zadane_cislo1[1]}{zadane_cislo1[2]}{zadane_cislo1[3]}")
+
+
+def seznam_zadane_cislo():
+    zadane_cislo1 = input("Enter a number:")
+    if len(zadane_cislo1) == 4:
+        print(oddelovac)
+    else:
+        print("the number doesnt have four numbers")
+        quit
+    global zadane_cislo1_seznam
+    zadane_cislo1_seznam = list(f"{zadane_cislo1[0]}{zadane_cislo1[1]}{zadane_cislo1[2]}{zadane_cislo1[3]}")
+
+
+# zajištění čtyřmístného čísla
+
+
+
+def pocitani_cow(cow = 0):
+    if zadane_cislo1_seznam[0] == nahodne_cislo[0]:
+        cow = cow + 1
+    if zadane_cislo1_seznam[1] == nahodne_cislo[1]:
+        cow = cow + 1
+    if zadane_cislo1_seznam[2] == nahodne_cislo[2]:
+        cow = cow + 1
+    if zadane_cislo1_seznam[3] == nahodne_cislo[3]:
+        cow = cow + 1
+    print("cow: ", cow)
+    if cow == 4:
+        print("Nahodné číslo bylo: ", nahodne_cislo, "vyhráls")
+        quit()
+# print(zadane_cislo1_seznam)
+
+def pocitani_bull(bull = 0):
+    if zadane_cislo1_seznam[0] == nahodne_cislo[1]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[0] == nahodne_cislo[2]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[0] == nahodne_cislo[3]:
+        bull = bull + 1
+    if zadane_cislo1_seznam[1] == nahodne_cislo[0]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[1] == nahodne_cislo[2]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[1] == nahodne_cislo[3]:
+        bull = bull + 1
+    if zadane_cislo1_seznam[2] == nahodne_cislo[0]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[2] == nahodne_cislo[1]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[2] == nahodne_cislo[3]:
+        bull = bull + 1
+    if zadane_cislo1_seznam[3] == nahodne_cislo[0]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[3] == nahodne_cislo[1]:
+        bull = bull + 1
+    elif zadane_cislo1_seznam[3] == nahodne_cislo[2]:
+        bull = bull + 1
+    print("bull: ", bull)
+
+while cow < 4:
+    seznam_zadane_cislo()
+    pocitani_cow()
+    pocitani_bull()
+
+
+
